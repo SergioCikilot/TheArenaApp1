3717379ffc06fb9680693f2be1331d7bc97cf6f9
sergio_cikilot <usnamoglu@hotmail.com>
2022-04-21 21:24:17 +0300

    ini

--- src/main/java/draft1/TheArenaApp1/TheArenaApp1Application.java
+++ src/main/java/draft1/TheArenaApp1/TheArenaApp1Application.java
@@ -1,33 +1,28 @@
 package draft1.TheArenaApp1;
 
-import draft1.TheArenaApp1.config.JdbcConfig;
 import draft1.TheArenaApp1.config.SwaggerConfig;
 
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 @SpringBootApplication
 
 public class TheArenaApp1Application {
 
-	public TheArenaApp1Application() throws SQLException {
+	public TheArenaApp1Application()  {
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheArenaApp1Application.class, args);
 	}
 
-	//JdbcConfig jdbcConfig = new JdbcConfig();
+
 
 	Docket swaggerConfig = new SwaggerConfig().api();
 
