e4d7ef95cf4f072fd0c5b6bec95a97a2d282b067
sergio_cikilot <usnamoglu@hotmail.com>
2022-05-09 20:09:27 +0300

    init

---
+++ src/main/java/draft1/TheArenaApp1/api/controllers/VersionController.java
@@ -0,0 +1,19 @@
+package draft1.TheArenaApp1.api.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/showCurrentVersion")
+    public String showCurrentVersion(){
+
+        return "Running Version: _v.0.5.5L";
+
+    }
+
+}
