8f28ccd31ceb623a654fc61005e9d1a2f2f99aa4
SergioCikilot <62554373+SergioCikilot@users.noreply.github.com>
2022-06-10 17:39:55 +0300

    init

--- src/main/java/draft1/TheArenaApp1/api/controllers/LoginController.java
+++ src/main/java/draft1/TheArenaApp1/api/controllers/LoginController.java
@@ -8,5 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 @CrossOrigin
 public class LoginController
-{
-}
+{}
---
+++ src/main/java/draft1/TheArenaApp1/config/WebCorsConfig.java
@@ -0,0 +1,23 @@
+package draft1.TheArenaApp1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+    @Configuration
+    @EnableWebMvc
+    public class WebCorsConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("https://arenahalisaha.azurewebsites.net/login")
+                    .allowedMethods("ADD","GET","PUT", "DELETE")
+                    .allowedHeaders("header1", "header2", "header3")
+                    .exposedHeaders("header1", "header2")
+                    .allowCredentials(false).maxAge(3600);
+        }
+    }
+
+
